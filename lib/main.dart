816adb6c2c35a460e6a193d21c414d9342ae7e71
Aby babu <abyb4613@gmail.com>
2025-12-16 21:40:31 +0530

    ff

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FruSette Admin ',
+      title: 'Frusette Admin ',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const SplashScreen(),
